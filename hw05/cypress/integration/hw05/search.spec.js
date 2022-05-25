3ffedd0a0f847281df51b184ae0d23f22457027a
Serg <sergstep314@gmail.com>
2022-05-25 05:44:28 +0400

    Fixes for HW05

--- hw05/cypress/integration/hw05/search.spec.js
+++ hw05/cypress/integration/hw05/search.spec.js
@@ -1,8 +1,7 @@
 ///<reference types='cypress'/>
 
 const locator_searchfield = "[id=idSearchBox]"
-const searchstring_success = "Mercedes"
-const searchstring_failure = "fignya"
+const locator_non_existent = '.notfound'
 
 describe('Test search', () => {
 
@@ -10,16 +9,53 @@ describe('Test search', () => {
 
         cy.visit('https://list.am/am/');
     })
-    it('Item found', () => {
+    it('Item valid 01', () =>  {
+        const searchString = "Mercedes"
 
-        cy.get(locator_searchfield).type(searchstring_success+'\n')
-        cy.get("div").contains(searchstring_success).first().should('exist')
+        cy.get(locator_searchfield).type(searchString+'\n')
+        cy.get(locator_non_existent).should('not.exist')
         
     })
-    it('Item not found', () => {
+    it('Item valid 02', () =>  {
+        const searchString = "mercedes գազ"
+
+        cy.get(locator_searchfield).type(searchString+'\n')
+        cy.get(locator_non_existent).should('not.exist')
         
-        cy.get(locator_searchfield).type(searchstring_failure+'\n')
-        cy.get("div").contains(searchstring_failure).should('not.exist')
+    })
+    it('Item valid 03', () =>  {
+        const searchString = "уроки русского"
+
+        cy.get(locator_searchfield).type(searchString+'\n')
+        cy.get(locator_non_existent).should('not.exist')
+        
+    })
+    it('Item valid 04', () => {
+        
+        const searchString = "mercedes գ"
+        cy.get(locator_searchfield).type(searchString+'\n')
+        cy.get(locator_non_existent).should('not.exist')
+        
+    })
+    it('Item valid 05', () =>  {
+        const searchString = "уроки рус"
+
+        cy.get(locator_searchfield).type(searchString+'\n')
+        cy.get(locator_non_existent).should('not.exist')
+        
+    })
+    it('Item valid 06', () =>  {
+        const searchString = "IpHoNe"
+
+        cy.get(locator_searchfield).type(searchString+'\n')
+        cy.get(locator_non_existent).should('not.exist')
+        
+    })
+    it('Item invalid 01', () =>  {
+        const searchString = "անհայտ բաներ"
+
+        cy.get(locator_searchfield).type(searchString+'\n')
+        cy.get(locator_non_existent).should('exist')
         
     })
   })
